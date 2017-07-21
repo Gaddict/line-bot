97baa687d1f15728648df74ac5da62239e7d3a67
佐野　航介 <ksano6253@gmail.com>
2017-07-21 15:58:36 +0900

    middleware

--- api/controllers/LinebotController.js
+++ api/controllers/LinebotController.js
@@ -5,40 +5,42 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+const line = require('@line/bot-sdk');
+
+const config = {
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+    channelSecret: process.env.CHANNEL_SECRET
+};
+
+const client = new line.Client(config);
+
+const middleware = line.middleware(config);
+
 module.exports = {
     callback: function (req, res) {
-        Promise
+        middleware(req, res)
         .all(req.body.events.map(handleEvent))
         .then((result) => {
             console.log(`result : ${result}`);
             return res.json(result);
         });
-        
-        function handleEvent(event) {
-            console.log(`event : ${event}`);
-
-            if (event.type !== 'message' && event.message.type !== 'text') {
-                return Promise.resolve(null);
-            }
-
-            const line = require('@line/bot-sdk');
-
-            const config = {
-                channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-                channelSecret: process.env.CHANNEL_SECRET
-            };
-
-            const client = new line.Client(config);
-
-            const reply = client.replyMessage(event.replyToken, {
-                type: 'text',
-                text: event.message.text
-            });
-
-            console.log(`reply : ${reply}`);
-
-            return reply;
-        }
-    },
+    }
 };
+
+function handleEvent(event) {
+    console.log(`event : ${event}`);
+
+    if (event.type !== 'message' && event.message.type !== 'text') {
+        return Promise.resolve(null);
+    }
+
+    const reply = client.replyMessage(event.replyToken, {
+        type: 'text',
+        text: event.message.text
+    });
+
+    console.log(`reply : ${reply}`);
+
+    return reply;
+}
 
