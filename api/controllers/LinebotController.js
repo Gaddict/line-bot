e5dc2295fbeae97e3cabb5152897d720bb5d4608
佐野　航介 <ksano6253@gmail.com>
2017-07-21 13:57:37 +0900

    test

--- api/controllers/LinebotController.js
+++ api/controllers/LinebotController.js
@@ -7,25 +7,24 @@
 
 module.exports = {
     callback: function (req, res) {
-        const line = require('@line/bot-sdk');
-
-        const config = {
-            channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-            channelSecret: process.env.CHANNEL_SECRET
-        };
-
-        console.log(config);
-
         Promise
         .all(req.body.events.map(handleEvent))
         .then((result) => res.json(result));
-
-        const client = new line.Client(config);
+        
         function handleEvent(event) {
             if (event.type !== 'message' || event.message.type !== 'text') {
                 return Promise.resolve(null);
             }
-      
+
+            const line = require('@line/bot-sdk');
+
+            const config = {
+                channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+                channelSecret: process.env.CHANNEL_SECRET
+            };
+            
+            const client = new line.Client(config);
+
             return client.replyMessage(event.replyToken, {
                 type: 'text',
                 text: event.message.text
